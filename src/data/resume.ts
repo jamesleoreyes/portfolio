975f85d8267ea376ec7ec9a43dfac4c2df5f37ff
jamesleoreyes <jamesleoreyes@gmail.com>
2025-10-02 21:28:45 -0400

    chore: Bump version to 0.20.9 and update completion progress for Boot.dev certification
    - Increment version number in package.json to 0.20.9
    - Update lastUpdate for Boot.dev certification to 10/2/2025
    - Increase completionProgress for Boot.dev certification from 27 to 30

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -117,8 +117,8 @@ const resumeData: ResumeData = {
       issuer: 'Boot.dev',
       issuerUrl: 'https://www.boot.dev/tracks/backend-python-typescript',
       date: 'In Progress',
-      lastUpdate: '10/1/2025',
-      completionProgress: 27
+      lastUpdate: '10/2/2025',
+      completionProgress: 30
     },
   ],
   technicalSkills: {
